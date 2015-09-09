9338a13b9f539835ff5b6999ab33bb632951a7ce
Adam Veary <adamjv90@gmail.com>
2015-09-09 15:26:52 -0400

    isomorphic fix

--- src/setSelection.js
+++ src/setSelection.js
@@ -1,34 +1,44 @@
 'use strict';
 
+var canUseDOM = !!(
+  typeof window !== 'undefined' &&
+  window.document &&
+  window.document.createElement
+);
+
 var getSelection = require('./getSelection');
 var rangeToTextRange = require('./rangeToTextRange');
-var doc = global.document;
+var setSelection;
 
-function setSelection (p) {
-  if (doc.createRange) {
-    modernSelection();
-  } else {
-    oldSelection();
-  }
+if (canUseDOM) {
+  var doc = global.document;
 
-  function modernSelection () {
-    var sel = getSelection();
-    var range = doc.createRange();
-    if (!p.startContainer) {
-      return;
-    }
-    if (p.endContainer) {
-      range.setEnd(p.endContainer, p.endOffset);
+  setSelection = function (p) {
+    if (doc.createRange) {
+      modernSelection();
     } else {
-      range.setEnd(p.startContainer, p.startOffset);
+      oldSelection();
     }
-    range.setStart(p.startContainer, p.startOffset);
-    sel.removeAllRanges();
-    sel.addRange(range);
-  }
 
-  function oldSelection () {
-    rangeToTextRange(p).select();
+    function modernSelection () {
+      var sel = getSelection();
+      var range = doc.createRange();
+      if (!p.startContainer) {
+        return;
+      }
+      if (p.endContainer) {
+        range.setEnd(p.endContainer, p.endOffset);
+      } else {
+        range.setEnd(p.startContainer, p.startOffset);
+      }
+      range.setStart(p.startContainer, p.startOffset);
+      sel.removeAllRanges();
+      sel.addRange(range);
+    }
+
+    function oldSelection () {
+      rangeToTextRange(p).select();
+    }
   }
 }
 
